9c703bc7864e48b3a6e64ba3bc7e11f52923e278
aiswarya-03-cloud <aiswaryaajith04@gmail.com>
2025-03-03 09:47:42 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,10 +2,10 @@ import jwt from 'jsonwebtoken'
 
 // import crypto from 'crypto'
 
-// // Generate a secure random JWT secret key
-// const jwtSecretKey = crypto.randomBytes(64).toString('base64');
+// Generate a secure random JWT secret key
+const jwtSecretKey = crypto.randomBytes(64).toString('base64');
 
-// console.log('Your JWT Secret Key:', jwtSecretKey);
+console.log('Your JWT Secret Key:', jwtSecretKey);
 
 
 
